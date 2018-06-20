80c699265052df118bf19b40661e9a89b6c70288
Lawrence Woodman <lwoodman@vlifesystems.com>
2018-06-20 08:02:45 +0100

    Move starRating into own plugin

--- plugins/posts.tcl
+++ plugins/posts.tcl
@@ -85,33 +85,6 @@ proc posts::makeRelated {posts post} {
   }]
 }
 
-# Displays the rating as a series of stars
-proc posts::starRating {rating} {
-  set starImgTag "<img src=\"/images/%s\" alt=\"%.1f/5.0\" />"
-
-  set wholeStars [expr {floor($rating)}]
-  if {$rating - $wholeStars > 0.5} {
-    incr wholeStars - 1
-  }
-
-  set halfStar [expr {($rating - $wholeStars == 0.5 ? 1 : 0)}]
-  set clearStars [expr {5 - ($wholeStars + $halfStar)}]
-
-  set htmlOutput ""
-  for {set i 0} {$i < $wholeStars} {incr i} {
-    append htmlOutput [format $starImgTag "star_filled.png" $rating]
-  }
-
-  if {$halfStar == 1} {
-    append htmlOutput [format $starImgTag "star_half.png" $rating]
-  }
-
-  for {set i 0} {$i < $clearStars} {incr i} {
-    append htmlOutput [format $starImgTag "star_clear.png" $rating]
-  }
-
-  return "$htmlOutput"
-}
 
 # Sort posts in decreasing date order
 proc posts::sort {posts} {
---
+++ plugins/starrating.tcl
@@ -0,0 +1,33 @@
+namespace eval starRating {
+  namespace export {[a-z]*}
+}
+
+# Displays the rating as a series of stars
+# Images msut be stored in [dir starImages]
+proc starRating::starRating {rating} {
+  set imagesURL [getvar plugins starRating imagesURL]
+  set starImgTag "<img src=\"$imagesURL/%s\" alt=\"%.1f/5.0\" />"
+
+  set wholeStars [expr {floor($rating)}]
+  if {$rating - $wholeStars > 0.5} {
+    incr wholeStars - 1
+  }
+
+  set halfStar [expr {($rating - $wholeStars == 0.5 ? 1 : 0)}]
+  set clearStars [expr {5 - ($wholeStars + $halfStar)}]
+
+  set htmlOutput ""
+  for {set i 0} {$i < $wholeStars} {incr i} {
+    append htmlOutput [format $starImgTag "star_filled.png" $rating]
+  }
+
+  if {$halfStar == 1} {
+    append htmlOutput [format $starImgTag "star_half.png" $rating]
+  }
+
+  for {set i 0} {$i < $clearStars} {incr i} {
+    append htmlOutput [format $starImgTag "star_clear.png" $rating]
+  }
+
+  return "$htmlOutput"
+}
